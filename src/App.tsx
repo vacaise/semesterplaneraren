8e10eed6e9632d47d040ca785675ed99042d3c18
vacaise <vacai.se@yahoo.com>
2025-04-04 14:23:22 +0200

    qweq

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-
-import { OnboardingContainer } from './components/features/onboarding/OnboardingContainer'
 import { OptimizerProvider } from './contexts/OptimizerContext'
+import { OptimizerForm } from './components/OptimizerForm'
 
 function App() {
+  const handleSubmit = (data: any) => {
+    console.log('Form submitted with data:', data);
+  };
+
   return (
     <OptimizerProvider>
       <div className="min-h-screen bg-white dark:bg-gray-900">
         <main className="container mx-auto px-4 py-8">
           <h1 className="text-3xl font-bold text-center mb-8">Holiday Optimizer</h1>
-          <OnboardingContainer />
+          <OptimizerForm onSubmitAction={handleSubmit} />
         </main>
       </div>
     </OptimizerProvider>
